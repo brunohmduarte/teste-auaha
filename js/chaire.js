5024fdab91a449afa1c29927f147e545c11e4bf5
Bruno Duarte <bruno_ocz86@hotmail.com>
2020-03-01 15:51:53 -0300

    Conclusão

--- js/chaire.js
+++ js/chaire.js
@@ -23,17 +23,6 @@ $(document).ready(function() {
         lazyLoad: 'ondemand',
     });
 
-    // Carrossel da vitrine.
-    $('.feeds-2 ul').slick({
-        dots: true,
-        arrows: true,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        lazyLoad: 'ondemand',
-    });
-
     // Substitui os icones(Prev/Next) do carrossel.
     $('.slick-prev').html('<i class="flaticon-left-arrow"></i>');
     $('.slick-next').html('<i class="flaticon-next"></i>');
